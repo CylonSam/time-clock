36fef37da794b60a73c963c0609de2a424dd5e2f
Samantha Silva <samanthaathayde@hotmail.com>
2024-02-24 17:03:58 -0300

    feat: add new routers

--- time_clock/main.py
+++ time_clock/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from .routers import employees, teams
+
+from .routers import work_schedules_router, employees_router, teams_router, calendar_router
 
 app = FastAPI(title="TimeClock")
 
-app.include_router(employees.router)
-app.include_router(teams.router)
+app.include_router(teams_router.router)
+app.include_router(employees_router.router)
+app.include_router(calendar_router.router)
+app.include_router(work_schedules_router.router)
